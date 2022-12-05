6993310de570925db0fafa3deecb87895cb2f655
MD SHAH SAMIR <mdshahsamir@gmail.com>
2022-12-05 15:25:35 +0600

    RepoListAdapter class created

---
+++ app/src/main/java/com/apps/therapassignment/ui/repolist/RepoListAdapter.kt
@@ -0,0 +1,32 @@
+package com.apps.therapassignment.ui.repolist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.apps.therapassignment.databinding.RepoLayoutBinding
+import com.apps.therapassignment.model.Repository
+import com.bumptech.glide.Glide
+
+class RepoListAdapter(private val repoList: ArrayList<Repository>) :
+    RecyclerView.Adapter<RepoListAdapter.ViewHolder>() {
+
+    inner class ViewHolder(val binding: RepoLayoutBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = RepoLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        with(holder.binding){
+            (repoList[position]).let { repo ->
+                repoName.text = repo.name
+                authorName.text = repo.owner.login
+                repoDescription.text = repo.description
+                Glide.with(imageView.context).load(repo.owner.avatar_url)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = repoList.size
+}
