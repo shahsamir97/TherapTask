e70f5e7bae248fef30bc41279c83edfab06363f0
MD SHAH SAMIR <mdshahsamir@gmail.com>
2022-12-06 18:02:17 +0600

    model classes properties made nullable

--- app/src/main/java/com/apps/therapassignment/model/License.kt
+++ app/src/main/java/com/apps/therapassignment/model/License.kt
@@ -1,9 +1,9 @@
 package com.apps.therapassignment.model
 
 data class License(
-    val key: String,
-    val name: String,
-    val node_id: String,
-    val spdx_id: String,
-    val url: String
+    val key: String?,
+    val name: String?,
+    val node_id: String?,
+    val spdx_id: String?,
+    val url: String?
 )
