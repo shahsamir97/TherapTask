ecbf1bccab263bd02f1f03b5f8b866bbf8732519
MD SHAH SAMIR <mdshahsamir@gmail.com>
2022-12-12 11:16:16 +0600

    MainActivity.kt moved to ui directory

--- app/src/main/java/com/apps/therapassignment/ui/MainActivity.kt
+++ app/src/main/java/com/apps/therapassignment/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.apps.therapassignment
+package com.apps.therapassignment.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.apps.therapassignment.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
