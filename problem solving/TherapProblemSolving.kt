00a69edbce82019fae1e62ab06ef3a0a65a3da83
Md shah samir <38814808+shahsamir97@users.noreply.github.com>
2022-12-13 20:59:44 +0600

    ProblemSet 2 Solved

---
+++ problem solving/TherapProblemSolving.kt
@@ -0,0 +1,32 @@
+fun main() {
+    val scanner = java.util.Scanner(System.`in`)
+    val countryA = scanner.nextLine()
+    val countryB = scanner.nextLine()
+    println(countryB + " " + countryA)
+    val matchedChar = bannerProblem(countryA, countryB)
+    println(matchedChar)
+}
+
+fun bannerProblem(a: String, b: String): String {
+    var hashMapA = HashMap<Char, Int>()
+    var hashMapB = HashMap<Char, Int>()
+    a.forEach {
+        hashMapA.put(it, 0)
+    }
+    b.forEach {
+        hashMapB.put(it, 0)
+    }
+
+    for (i in 0 until  hashMapA.size) {
+        if (i < a.length && i < b.length) {
+            if (hashMapA.containsKey(b.get(i))) {
+                return b.get(i).toString()
+            }
+            if (hashMapB.containsKey(a.get(i)))
+                return a.get(i).toString()
+        }else{
+            return "No match found"
+        }
+    }
+    return "No match found"
+}
