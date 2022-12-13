8921ba104bc072bbed72eba1ccb3d363e15a468c
MD SHAH SAMIR <mdshahsamir@gmail.com>
2022-12-13 10:17:05 +0600

    redundant imports removed from MainActivity.kt

--- app/src/main/java/com/apps/therapassignment/ui/MainActivity.kt
+++ app/src/main/java/com/apps/therapassignment/ui/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.apps.therapassignment.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.FragmentContainerView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.apps.therapassignment.R
